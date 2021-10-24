8a0b1ec17a5998cde9af36273032aacef2fcfec4
liuch <liuch@mail.ru>
2021-10-24 14:49:22 +0300

    bump version

--- classes/Core.php
+++ classes/Core.php
@@ -39,7 +39,7 @@ use Exception;
  */
 class Core
 {
-    public const APP_VERSION = '1.1';
+    public const APP_VERSION = '1.2';
     private const SESSION_NAME = 'session';
     private static $html_file_name = 'index.html';
     private static $v_auth = null;
