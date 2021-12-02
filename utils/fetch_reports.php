1b1a0b46aacd9c0534fa046ba243aa11e49e86ee
liuch <liuch@mail.ru>
2021-12-02 23:03:28 +0300

    Updated fetch_reports.php script
    - Added the ability to load report files from the local server directories
    - An optional parameter was added to specify the type of report file source to load

--- utils/fetch_reports.php
+++ utils/fetch_reports.php
@@ -21,10 +21,19 @@
  *
  * =========================
  *
- * This script fetches DMARC reports from mailboxes and saves them to the DB.
- * The mailbox parameters must be specified in the configuration file.
- * The mailbox accessibility can be checked on the administration page in
- * the web interface.
+ * This script fetches DMARC reports from mailboxes and server local directories and saves them to the DB.
+ * The parameters of mailboxes and directories must be specified in the configuration file.
+ * The mailboxes and directories accessibility can be checked on the administration page in the web interface.
+ * The script has one optional parameter: `source` - the type of the source. The valid types are `email`, `directory`.
+ *
+ * Some examples:
+ * $ utils/fetch_reports
+ * will fetch reports from both the mailboxes and the local server directories.
+ *
+ * $ utils/fetch_reports source=email
+ * will only fetch reports from the mailboxes.
+ *
+ * The best place to use it is cron.
  * Note: the current directory must be the one containing the classes directory.
  *
  * @category Utilities
@@ -38,6 +47,8 @@ namespace Liuch\DmarcSrg;
 use Liuch\DmarcSrg\Mail\MailBoxes;
 use Liuch\DmarcSrg\Report\ReportFetcher;
 use Liuch\DmarcSrg\Sources\MailboxSource;
+use Liuch\DmarcSrg\Sources\DirectorySource;
+use Liuch\DmarcSrg\Directories\DirectoryList;
 
 require 'init.php';
 
@@ -46,15 +57,58 @@ if (php_sapi_name() !== 'cli') {
     exit(1);
 }
 
+$usage  = false;
+$source = null;
+if (isset($argv)) {
+    for ($i = 1; $i < count($argv); ++$i) {
+        $av = explode('=', $argv[$i]);
+        if (count($av) !== 2) {
+            echo 'Invalid parameter format' . PHP_EOL;
+            $usage = true;
+            break;
+        }
+        switch ($av[0]) {
+            case 'source':
+                $source = $av[1];
+                break;
+            default:
+                echo 'Unknown parameter "' . $av[0] . '"' . PHP_EOL;
+                $usage = true;
+                break;
+        }
+    }
+    if ($source && $source !== 'email' && $source !== 'directory') {
+        echo 'Invalid source type "' . $source . '". "email" or "directory" expected.' . PHP_EOL;
+        exit(1);
+    }
+}
+
+if ($usage) {
+    echo PHP_EOL;
+    echo 'Usage: ' . basename(__FILE__) . ' [source=email|directory]' . PHP_EOL;
+    exit(1);
+}
+
 $sou_list = [];
 $messages = [];
 
-$mb_list = new MailBoxes();
-for ($mb_num = 1; $mb_num <= $mb_list->count(); ++$mb_num) {
-    try {
-        $sou_list[] = new MailboxSource($mb_list->mailbox($mb_num));
-    } catch (\Exception $e) {
-        $messages[] = $e->getMessage();
+if (!$source || $source === 'email') {
+    $mb_list = new MailBoxes();
+    for ($mb_num = 1; $mb_num <= $mb_list->count(); ++$mb_num) {
+        try {
+            $sou_list[] = new MailboxSource($mb_list->mailbox($mb_num));
+        } catch (\Exception $e) {
+            $messages[] = $e->getMessage();
+        }
+    }
+}
+if (!$source || $source === 'directory') {
+    foreach ((new DirectoryList())->list() as $dir) {
+        try {
+            $sou_list[] = new DirectorySource($dir);
+        } catch (\Exception $e) {
+            $messages[] = $e->getMessage();
+        }
     }
 }
 
