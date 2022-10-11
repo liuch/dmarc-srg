abc2edee2558557f2f9170263bdca70303f756a6
Stefan Meinecke <meinecke@greensec.de>
2022-10-11 14:59:57 +0200

    https://github.com/smeinecke/dmarc-srg/issues/5 reformat stat-block

--- js/status.js
+++ js/status.js
@@ -99,7 +99,7 @@ class Status {
 		let passed = this._data.emails.dkim_spf_aligned;
 		let forwarded = this._data.emails.dkim_aligned + this._data.emails.spf_aligned;
 		let failed = total - passed - forwarded;
-		this._set_element_data("processed", total === -1 && "?" || total, total !== -1 && "state-blue" || null);
+		this._set_element_data("processed", total === -1 && "?" || total.toLocaleString(), total !== -1 && "state-blue" || null);
 		this._set_element_data("passed", this._formatted_statistic(passed, total), total !== -1 && "state-green" || null);
 		this._set_element_data("forwarded", this._formatted_statistic(forwarded, total), total !== -1 && "state-green" || null);
 		this._set_element_data("failed", this._formatted_statistic(failed, total), total !== -1 && "state-red" || null);
