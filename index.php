9000543ded6ee8ee4059a846ef5a219773217bcf
liuch <liuch@mail.ru>
2023-11-19 00:14:52 +0300

    tiny fix in the old index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<h1>DMARC Reports</h1>
-    <p>This page is obsolete. Please change the path to the html root directory in your web server settings.</p>
-    <p>You can follow this <a href="public/" title="DMARC Reports">link</a> to continue.</p>
+	<p>This page is obsolete. Please change the path to the html root directory in your web server settings.</p>
+	<p>You can follow this <a href="public/" title="DMARC Reports">link</a> to continue.</p>
 </body>
 </html>
