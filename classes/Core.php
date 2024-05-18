c306990edbab0535efbf6a9c4c141c5e5de9d4a3
liuch <liuch@mail.ru>
2024-05-18 16:32:39 +0300

    bump version

--- classes/Core.php
+++ classes/Core.php
@@ -45,7 +45,7 @@ use Liuch\DmarcSrg\Exception\ForbiddenException;
  */
 class Core
 {
-    public const APP_VERSION = '2.0';
+    public const APP_VERSION = '2.1';
 
     private const SESSION_NAME = 'session';
 
