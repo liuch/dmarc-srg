95fd09f82ee06f17b6fee220bce972b0afcd27cb
Aleksey Andreev <liuch@mail.ru>
2025-03-21 01:15:50 +0300

    code style fix in mailbox_cleaner.php

--- utils/mailbox_cleaner.php
+++ utils/mailbox_cleaner.php
@@ -88,8 +88,7 @@ foreach ([ [ 'done', '', 1 ], [ 'failed', 'failed', 0 ] ] as $it) {
             if ($act->type === SourceAction::ACTION_MOVE) {
                 $dir = $act->param;
                 break;
-            }
-            elseif ($act->type === SourceAction::ACTION_SEEN) {
+            } elseif ($act->type === SourceAction::ACTION_SEEN) {
                 $dir = $def_opt;
             }
         }
